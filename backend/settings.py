b781fea14dcd02894382da8239a16a1f134f6525
mohit <mohit21339@iiitd.ac.in>
2024-08-07 19:56:32 +0530

    added onrender to ALLWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ".vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'course-compass-backend.onrender.com']
 
 
 # Application definition
